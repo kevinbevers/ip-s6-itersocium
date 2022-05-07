5c2e85f431a23e9bd875db449cce3fbcd7789f43
Kevin Bevers <kevin.bevers@hotmail.com>
2022-05-07 21:06:05 +0200

    added first iteration of popular list (no api)

---
+++ App/consts/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+  white: '#FFF',
+  dark: '#000',
+  primary: '#26c731',
+  secondary: '#e1e8e9',
+  light: '#f9f9f9',
+  grey: '#dddedd',
+  red: 'red',
+  orange: '#f5a623',
+};
+
+export default COLORS;
