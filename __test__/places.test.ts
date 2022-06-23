4f14de5a243e60f72cc6fac46727770e8da59a6f
Kevin Bevers <kevin.bevers@hotmail.com>
2022-06-23 11:30:54 +0200

    added another expect

--- __test__/places.test.ts
+++ __test__/places.test.ts
@@ -3,7 +3,7 @@ require("isomorphic-fetch");
 
 describe("Places Tests", () => {
   it("should get return all 5 migrated places", async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     try {
       const url = new URL(process.env.HASURA_ENDPOINT ?? "")
       const response = await fetch(url, {
@@ -27,6 +27,7 @@ describe("Places Tests", () => {
       });
       const { data } = await response.json();
       expect(data.places.length).toEqual(5);
+      expect(data.places[0].location).toBe("Italy");
     } catch (error) {
       console.log(error);
     }
